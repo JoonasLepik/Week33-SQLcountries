560ac7c20cffa3b358cd83ddebbc7b11052a0671
Opilane TTHK <opilane@tthk.ee>
2022-02-18 10:57:32 +0200

    SQL

---
+++ Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source=Countries.db");
+
+        }
+        public void OpenConnection()
+        {
+            if(myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
